9c49da4b31382d4212c2594e492441792c23c314
Thomas Pons <pons.th@gmail.com>
2014-04-01 16:57:50 +0200

    key

--- app/scripts/constants/api.js
+++ app/scripts/constants/api.js
@@ -2,7 +2,7 @@
 
 angular.module('condingDojoApp')
     .constant('Api', {
-        KEY: '2b550ece830a7822d22aad53b2383e2e',
+        KEY: 'XXXX',
         BASE: 'http://gateway.marvel.com/v1/public'
     });
 
