b86eedba9bbd80b5c5ed393a4bd543560868c721
Thomas Pons <pons.th@gmail.com>
2014-04-15 13:55:09 +0200

    remove key again

--- app/scripts/constants/api.js
+++ app/scripts/constants/api.js
@@ -2,7 +2,7 @@
 
 angular.module('codingDojoApp')
     .constant('Api', {
-        KEY: '2b550ece830a7822d22aad53b2383e2e',
+        KEY: 'XXXXX',
         BASE: 'http://gateway.marvel.com/v1/public'
     });
 
